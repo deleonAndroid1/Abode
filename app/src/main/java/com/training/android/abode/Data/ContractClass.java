7552536029fede153eda4280ca9e0f0f7887335f
hillarybriones <hillarysabanatebriones@gmail.com>
2017-03-19 21:58:50 +0800

    partial contract churva mo crash pani

---
+++ app/src/main/java/com/training/android/abode/Data/ContractClass.java
@@ -0,0 +1,24 @@
+package com.training.android.abode.Data;
+
+/**
+ * Created by Hillary Briones on 3/19/2017.
+ */
+
+public class ContractClass {
+private String imgUrl;
+
+    public ContractClass() {
+    }
+
+    public ContractClass(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
